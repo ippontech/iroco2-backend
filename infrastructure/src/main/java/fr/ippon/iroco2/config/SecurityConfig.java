6518ce83fc200d0573231e6cd954cdc1ff4c38dd
npochic <npochic@ippon.fr>
2025-07-28 10:01:34 +0200

    refactor(infra&legacy): move SecurityConfig

--- infrastructure/src/main/java/fr/ippon/iroco2/config/SecurityConfig.java
+++ infrastructure/src/main/java/fr/ippon/iroco2/config/SecurityConfig.java
@@ -15,7 +15,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package fr.ippon.iroco2.legacy.config;
+package fr.ippon.iroco2.config;
 
 import fr.ippon.iroco2.access.presentation.JwtAuthenticationFilter;
 import fr.ippon.iroco2.access.presentation.ScannerAuthenticationFilter;
