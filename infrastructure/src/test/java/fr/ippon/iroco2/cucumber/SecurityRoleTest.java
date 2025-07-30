ac3ee68546cfc85e4179796074275ae831dee4be
npochic <npochic@ippon.fr>
2025-07-30 16:57:08 +0200

    fix(infrastructure): correct package name

--- infrastructure/src/test/java/fr/ippon/iroco2/cucumber/SecurityRoleTest.java
+++ infrastructure/src/test/java/fr/ippon/iroco2/cucumber/SecurityRoleTest.java
@@ -15,7 +15,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package fr.ippon.iroco2.legacy.access.domain;
+package fr.ippon.iroco2.cucumber;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
